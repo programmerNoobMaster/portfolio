734aa3bb3444dbdc66f848b63d08f59519a3f11e
Shreyas <shreyasacharya8@gmail.com>
2024-09-09 14:29:28 +0100

    first commit

---
+++ portfoliopers/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ResponsiveAppBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
